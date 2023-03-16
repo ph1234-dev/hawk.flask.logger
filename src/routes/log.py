5527aeb3169d76a196005ba1ffbaffab03fce862
ph1234-Hawk <35072905+ph1234-dev@users.noreply.github.com>
2023-03-16 13:34:00 +0800

    added new routes and updated queries

--- src/routes/log.py
+++ src/routes/log.py
@@ -1,6 +1,6 @@
-from flask import request, Blueprint, render_template,jsonify
+from flask import request, Blueprint, render_template,jsonify,redirect,url_for
 from config import db, app,validate_content_type_as_json,token_required
-from models.models import Log
+from models.models import Log,User
 from sqlalchemy.exc import IntegrityError,NoForeignKeysError
 
 
@@ -15,7 +15,19 @@ site = Blueprint('site',__name__)
 log.register_blueprint(api)
 log.register_blueprint(site)
 
-@api.route("/post",methods=["post"])
+
+
+# DEFAULT TABLE SETTINGS
+table_log_column_settings = TABLE_LOG_COLUMN_SETTINGS_DEFAULT = {
+    "no": True,
+    "user_id": False,
+    "message": False,
+    "reply": False,
+    "created_at": True
+}
+
+
+@api.route("/post",methods=["POST"])
 @validate_content_type_as_json
 @token_required
 def post_log():
@@ -39,15 +51,6 @@ def post_log():
     return jsonify(msg="Log has been added")
     
 
-
-# https://stackoverflow.com/questions/19261833/what-is-an-endpoint-in-flask
-@site.route("/recent",endpoint="show_recent_logs")
-def view_recent_logs():
-    count = 5
-    logs = Log.query.order_by(Log.created_at.desc()).limit(count).all()
-    return render_template('views/logs.jinja', logs=logs,count=count)
-    
-
 @api.route("/login",methods=['GET'],endpoint="")
 def view_login():
     if request.json:
@@ -55,3 +58,182 @@ def view_login():
         _password = request.get("password")
     else:
         return "Invalid data received"
+
+
+# SITE ROUTES
+
+
+# https://stackoverflow.com/questions/19261833/what-is-an-endpoint-in-flask
+@site.route("/",endpoint="show_logs")
+def view_recent_logs():
+    # read this since we have to use sqlalchmy actual
+    # https://docs.sqlalchemy.org/en/20/tutorial/data_select.html
+    table_log_column_settings = TABLE_LOG_COLUMN_SETTINGS_DEFAULT
+    logs =  db.paginate(
+                db.select(Log).order_by(Log.created_at.desc()),
+                per_page=10)
+    return render_template('views/logs.jinja', logs=logs)
+
+
+
+@site.route("/column/<int:column>",endpoint="sort_columns")
+def view_ascending_order(column):
+    # python switch
+    # https://www.freecodecamp.org/news/python-switch-statement-switch-case-example/
+    
+    if column == 0:
+        reverse = not table_log_column_settings["no"] 
+        table_log_column_settings["no"] = reverse 
+
+        if reverse == False:
+            logs = db.paginate(db.select(Log).order_by(Log.no.asc()), per_page=10)
+        else:
+            logs = db.paginate(db.select(Log).order_by(Log.no.desc()),per_page=10)
+            
+    elif column == 1:
+        # sort user_id
+        
+        reverse = not table_log_column_settings["user_id"] 
+        table_log_column_settings["user_id"] = reverse 
+
+        if reverse == False:
+            logs = db.paginate(db.select(Log).order_by(Log.user_id.asc()),per_page=10)
+        else:
+            logs = db.paginate(db.select(Log).order_by(Log.user_id.desc()), per_page=10)
+        
+    elif column == 2:
+        # sort messages
+        
+        reverse = not table_log_column_settings["message"] 
+        table_log_column_settings["message"] = reverse 
+
+        if reverse == False:
+            logs = db.paginate( db.select(Log).order_by(Log.message.asc()), per_page=10)
+        else:
+            logs = db.paginate( db.select(Log).order_by(Log.message.desc()),per_page=10)
+            
+    elif column == 3:
+
+        reverse = not table_log_column_settings["reply"] 
+        table_log_column_settings["reply"] = reverse 
+
+        if reverse == False:
+            logs = db.paginate(db.select(Log).order_by(Log.reply.asc()), per_page=10)
+        else:
+            logs = db.paginate( db.select(Log).order_by(Log.reply.desc()), per_page=10)
+            
+    elif column == 4:
+        reverse = not table_log_column_settings["created_at"] 
+        table_log_column_settings["created_at"] = reverse 
+
+        if reverse == False:
+            logs = db.paginate( db.select(Log).order_by(Log.created_at.asc()), per_page=10)
+        else:
+            logs = db.paginate( db.select(Log).order_by(Log.created_at.desc()),per_page=10)
+        
+    return render_template('views/logs.jinja', logs=logs)
+
+
+
+@site.route("/delete/<no>",endpoint="delete_log")
+def delete_log(no):
+    # return " i am deleting " + no
+    target = Log.query.filter_by(no=no).first()
+    # return f"log {target}"
+    try:
+        db.session.delete(target)
+        db.session.commit()
+        return redirect(url_for('log.site.show_logs'))
+    except:
+        db.session.rollback()
+        return "A problem occured when deleting log"
+
+
+@site.route("/users",endpoint="show_available_users")
+def get_users():
+    users = db.paginate(
+        db.select(User).order_by(User.created_at.desc()),
+        per_page=10)
+    return render_template('./views/users_available_for_creating_log.jinja',users=users)
+
+@site.route("/create/<id>",endpoint="create_log",methods=['POST','GET'])
+def create_user_log(id):
+
+    if request.method == 'GET':
+
+        return render_template('./views/create_log.jinja',target_id=id)
+    
+    elif request.method == 'POST':
+
+        _message = request.form.get('message')
+        _reply = request.form.get('reply')
+        _lang = request.form.get('lang') 
+
+        # if not _message.strip():
+        #     return render_template('./views/create_log',
+        #                 id=id,
+        #                 error="User message cannot be empty")
+
+        # if not _reply.strip():
+        #     return url_for('log.site.create_log',
+        #                 id=id,
+        #                 error="Chatbot reply cannot be empty")
+
+
+        # if not _lang.strip():
+        #     return url_for('log.site.create_log',
+        #                 id=id,
+        #                 error="Predicted language cannot be empty")
+
+        log = Log(
+            user_id=id,
+            message=_message,
+            reply=_reply,
+            predicted_message_language=_lang
+            )
+        
+        # print( f"User id: {id} / message: {_message} / reply: {_reply} / lang: {_lang}")
+        try:
+            db.session.add(log)
+            db.session.commit()
+            return redirect(url_for('log.site.create_log',id=id))
+        except:
+            db.session.rollback()
+            return "Something went wrong. Could not store log"
+        
+
+
+@site.route("/user_specific_logs/<id>",methods=['GET'],endpoint="get_user_specific_logs")
+def get_user_specific_logs(id):
+    # logs = Log.query.filter_by(user_id=id).all()
+    # logs = User.query.join(Log,User.id==Log.user_id).all()
+
+    # https://www.kevin7.net/post_detail/flask-sqlalchemy-simple-queries
+    # join query
+
+    # logs = db.session.query(User).join(Log,Log.user_id==User.id).filter(User.id==id).all()
+
+    # you can actually access the user items when joining by dotting .user 
+    # logs = db.session.query(Log).join(User,Log.user_id==User.id).filter(User.id==id).all()
+    user = User.query.filter_by(id=id).first()
+
+    logs = db.paginate(
+                db.select(Log)
+                    .join(User,Log.user_id==User.id)
+                    .filter(User.id==id)
+                    .order_by(Log.created_at.desc()),
+                per_page=10)
+    
+
+    return render_template('./views/user_specific_logs.jinja',logs=logs,user=user)
+
+
+@site.route('/search',methods=['GET','POST'],endpoint="search_by_user")
+def search_log_by_user():
+    id = request.form.get("id")
+    logs = db.paginate(
+                db.select(Log)
+                    .filter(Log.user_id==id)
+                    .order_by(Log.created_at.desc()),
+                per_page=10)
+    return render_template('views/logs.jinja', logs=logs)
--- src/routes/user.py
+++ src/routes/user.py
@@ -1,4 +1,5 @@
-from flask import request, Blueprint, render_template,jsonify, make_response
+from flask import request, Blueprint, render_template,jsonify, make_response,redirect,url_for
+
 from config import db,app,validate_content_type_as_json
 from models.models import User
 from sqlalchemy.exc import IntegrityError,NoForeignKeysError
@@ -24,16 +25,30 @@ site = Blueprint('site',__name__)
 user.register_blueprint(api)
 user.register_blueprint(site)
 
+
+# DEFAULT TABLE SETTINGS
+table_user_column_settings = TABLE_USER_COLUMN_SETTINGS_DEFAULT = {
+    "id": True,
+    "name": False,
+    "username": False,
+    "password": False,
+    "created_at": True
+}
+
+
+
+# API ROUTES
+
 # https://stackoverflow.com/questions/5868786/what-method-should-i-use-for-a-login-authentication-request
 # login must be post request
-@api.route("/login",methods=["post"])
+@api.route("/login",methods=["POST"])
 @validate_content_type_as_json
 def login():
     req = request.json
 
     user = User.query.filter_by(
-        username= req.get('username'),
-        password= req.get('password'),
+        username= req.get('username').strip(),
+        password= req.get('password').strip(),
     ).first()
         
     if user is None:
@@ -60,17 +75,18 @@ def login():
             id=user.id,
             name=user.name,
             token=user_token)
+    
 
 # Doing POST 
 # https://sentry.io/answers/flask-getting-post-data/
-@api.route("/register",methods=["post"])
+@api.route("/register",methods=["POST"])
 @validate_content_type_as_json
 def register():
     req = request.json
     user = User(
-        username=req.get('username'),
-        password=req.get('password'),
-        name=req.get('name')
+        username=req.get('username').strip(),
+        password=req.get('password').strip(),
+        name=req.get('name').strip()
         )
 
     # read this for exception handling
@@ -100,9 +116,190 @@ def register():
         # use the generic error code 500 for database errors
         return "The User already exist", 500
 
-@site.route("/records",endpoint="show_users")
+
+
+@site.route("/",endpoint="show_users")
 def view_recent_logs():
-    user = User.query.order_by(User.created_at.desc()).limit(5).all()
-    count = User.query.count()
-    return render_template('views/user.jinja', users=user,count=count)
+    # user = User.query.order_by(User.created_at.desc()).limit(5).all()
+    # count = User.query.count()
+
+    table_user_column_settings = TABLE_USER_COLUMN_SETTINGS_DEFAULT
+    page = db.paginate(db.select(User).order_by(User.created_at.desc()),per_page=10)
+    return render_template('views/user.jinja', users=page)
     
+
+@site.route("/column/<int:column>",endpoint="sort_columns")
+def sort_user_columns(column):
+    match column:
+        case 0: 
+            reverse = not table_user_column_settings['id']
+            table_user_column_settings['id'] = reverse
+
+            if table_user_column_settings['id'] == False:
+                page = db.paginate(db.select(User).order_by(User.id.desc()),per_page=10)
+            else:
+                page = db.paginate(db.select(User).order_by(User.id.asc()),per_page=10)
+        case 1:
+            reverse = not table_user_column_settings['name']
+            table_user_column_settings['name'] = reverse
+            
+            if table_user_column_settings['name'] == False:
+                page = db.paginate(db.select(User).order_by(User.name.asc()),per_page=10)
+            else:
+                page = db.paginate(db.select(User).order_by(User.name.desc()),per_page=10)
+        
+        case 2:
+            reverse = not table_user_column_settings['username']
+            table_user_column_settings['username'] = reverse
+            
+            if table_user_column_settings['username'] == False:
+                page = db.paginate(db.select(User).order_by(User.username.asc()),per_page=10)
+            else:
+                page = db.paginate(db.select(User).order_by(User.username.desc()),per_page=10)
+        case 3:
+            reverse = not table_user_column_settings['password']
+            table_user_column_settings['password'] = reverse
+            
+            if table_user_column_settings['password'] == False:
+                page = db.paginate(db.select(User).order_by(User.password.asc()),per_page=10)
+            else:
+                page = db.paginate(db.select(User).order_by(User.password.desc()),per_page=10)
+        case 4:
+            reverse = not table_user_column_settings['created_at']
+            table_user_column_settings['created_at'] = reverse
+            
+            if table_user_column_settings['created_at'] == False:
+                page = db.paginate(db.select(User).order_by(User.created_at.asc()),per_page=10)
+            else:
+                page = db.paginate(db.select(User).order_by(User.created_at.desc()),per_page=10)
+    
+        case default:
+            return "something went wrong"
+
+    return render_template('views/user.jinja', users=page)
+
+    
+    
+@site.route("/users",methods=['GET','POST'],endpoint="show_searched_user")
+def get_searched_user():
+    id = request.form.get("id")
+    target = db.paginate(db.select(User).filter_by(id=id))
+    return render_template('./views/users_available_for_creating_log.jinja',users=target)
+
+
+@site.route("/delete/<id>",endpoint="user_delete")
+def delete_user(id):
+    target = User.query.get(id)
+
+    try:
+        db.session.delete(target)
+        db.session.commit()
+        return redirect(url_for('user.site.show_users'))
+    except:
+        db.session.rollback()
+        return "A problem occured when deleting user"
+    
+
+
+
+# BEGIN SITE ROUTES
+
+
+# methods is type sensitive use CAPTIALIZE FOR GET/POST/PUT/PATCH
+@site.route("/update/<id>",methods=['GET','POST'],endpoint="user_update")
+def update_user(id):
+    # target = User.query.filter_by(id='').first()
+    
+    if  request.method == 'GET':
+        # render update ui
+        target = User.query.get(id)
+        return render_template('./views/update_user.jinja',user=target)
+    elif request.method == 'POST':
+        # render update successful
+        target = User.query.get(id)
+    
+        _username = request.form.get('username')
+        _password = request.form.get('password')
+        _name = request.form.get('name')
+
+        # target.username = _username
+        # if not _username.strip():
+        #     return render_template('./views/update_user.jinja',
+        #                            user=target,
+        #                            error="Username cannot be empty") 
+        
+        # target.password = _password
+        # if not _password.strip():
+        #     return render_template('./views/update_user.jinja',
+        #                            user=target,
+        #                            error="Password cannot be empty") 
+        
+        # allow name to be empty but not encouraged 
+        target.name = _name
+        target.username = _username
+        target.password = _password
+
+        # print ( f"Target: {target} / Old name: {target.name} / New name: {new_username}")
+        try:
+            # this all you need
+            db.session.commit()
+            return redirect(url_for('user.site.show_users'))
+        except:
+            db.session.rollback()
+            return "Something went wrong. Update was not performed"
+
+
+        # return 'Successful update'
+
+@site.route('/create',endpoint="user_create",methods=['GET','POST'])
+def create():
+    
+    if request.method == 'GET':
+        # render_template finds the 
+        # template folder . this is a default behavior from flask
+        return render_template('./views/create_user.jinja',user=None)
+    elif request.method == 'POST':
+        
+        _username = request.form.get('username')
+        _password = request.form.get('password')
+        _name = request.form.get('name')
+
+
+        # if not _username.strip():
+        #     return render_template('./views/create_user.jinja',
+        #                                error="username cant be empty")
+        
+        # if not _password.strip():
+        #     return render_template('./views/create_user.jinja',
+        #                                error="password cant be empty")
+            
+
+        
+        target = User.query.filter_by(username=_username).first()
+        # check if user exist
+
+        # means there is a user that goes by the name user
+        if target: 
+            # return "target exist"
+            return render_template(
+                './views/create_user.jinja',
+                error=f"Username {_username} already exist.\
+                    Please choose a differnet one")
+                    
+        #         # \ is a part of a new syntax which indicates a split and
+        #         # does not break the string
+        else:
+            
+            _user = User(
+                username=_username,
+                password=_password,
+                name=_name
+                )
+            
+            try:
+                db.session.add(_user)
+                db.session.commit()
+                return redirect(url_for('user.site.show_users'))
+            except:
+                db.session.rollback()
+                return render_template('./views/create_user.jinja',user=_user)
